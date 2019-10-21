6b6b1c89a94c8617b5eb75c0a9941c834d2939e9
Belinda Lei <belinda.lei95@gmail.com>
2019-10-21 16:03:07 -0700

    forms...will switch to a branch

--- src/components/NewOutfitForm.js
+++ src/components/NewOutfitForm.js
@@ -1,62 +1,91 @@
 import React from 'react'
 
-const FormContainer = (props) => {
+class FormContainer extends React.Component {
 
-  return (
-    <form>
-      Create a new top or bottom:
-      <br />
-      <label>
-        Top:
-          <input
-          name="Top"
-          type="radio"
-          checked={null}
-          onChange={null} />
-        Bottom:
-          <input
-          name="Bottom"
-          type="radio"
-          checked={null}
-          onChange={null} />
-      </label>
-      <br />
-      <label>
-        Category:
-          <select value={null} onChange={null}>
-          <option value="long sleeve">Long Sleeve</option>
-          <option value="short sleeve">Short Sleeve</option>
-          <option value="jeans">Jeans</option>
-          <option value="skirt">Skirt</option>
-          <option value="pants">Pants</option>
-        </select>
-      </label>
-      <label>
+  state = {
+    top: true,
+    category: "",
+    color: "",
+    img_url: "" 
+  }
+
+  changeHandler = e => {
+    console.log(e.target.name)
+    // if (e.target.name === "notVegetarian") {
+    //   this.setState({ vegetarian: false })
+    // } else if (e.target.name === "Vegetarian") {
+    //   this.setState({ vegetarian: true })
+    // } else {
+    //   let value = e.target.value;
+    //   this.setState({
+    //     [e.target.name]: value
+    //   });
+    // }
+  };
+
+  render(){
+    return (
+      <form onSubmit={this.props.submitHandler}>
+        Create a new top or bottom:
         <br />
-        Pick the color:
-          <select value={null} onChange={null}>
-          <option value="Black">Black</option>
-          <option value="Blue">Blue</option>
-          <option value="Green">Green</option>
-          <option value="Grey">Grey</option>
-          <option value="Orange">Orange</option>
-          <option value="Purple">Purple</option>
-          <option value="Red">Red</option>
-          <option value="White">White</option>
-          <option value="Yellow">Yellow</option>
-        </select>
-      </label>
-      <br />
-      <label>
-        Image URL:
-          <input
-          name="img_url"
-          type="text"
-          value={null}
-          onChange={null} />
-      </label>
-    </form>
-  )
+        <label>
+          Top:
+            <input
+            name="top"
+            value="top"
+            type="radio"
+            checked={this.state.top}
+            onChange={this.changeHandler} />
+          Bottom:
+            <input
+            name="bottom"
+            type="radio"
+            value="bottom"
+            checked={!this.state.top}
+            onChange={this.changeHandler} />
+        </label>
+        <br />
+        <label>
+          Category:
+            <select value={this.state.category} onChange={this.changeHandler}>
+            <option value="long sleeve">Long Sleeve</option>
+            <option value="short sleeve">Short Sleeve</option>
+            <option value="jeans">Jeans</option>
+            <option value="skirt">Skirt</option>
+            <option value="pants">Pants</option>
+          </select>
+        </label>
+        <label>
+          <br />
+          Pick the color:
+            <select value={null} onChange={this.changeHandler}>
+            <option value="Black">Black</option>
+            <option value="Blue">Blue</option>
+            <option value="Green">Green</option>
+            <option value="Grey">Grey</option>
+            <option value="Orange">Orange</option>
+            <option value="Purple">Purple</option>
+            <option value="Red">Red</option>
+            <option value="White">White</option>
+            <option value="Yellow">Yellow</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Image URL:
+            <input
+            name="img_url"
+            type="text"
+            value={null}
+            onChange={this.changeHandler} />
+        </label>
+        <button
+          type="submit"
+          className="ui pink button"
+        >Submit</button>
+      </form>
+    )
+  }
 }
 
 export default FormContainer;
