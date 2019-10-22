7cccfee2ac24a0eb8c3ac0f49c6079a49a180472
Belinda Lei <belinda.lei95@gmail.com>
2019-10-22 13:57:35 -0700

    Welcome route fleshed out

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,41 @@
-import React, { Component } from 'react';
+import React from "react";
+import SignUp from './SignUp'
+import LogIn from './LogIn'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const Welcome = () => {
-  return <h1>WELCOME TO CLUELESS STYLING</h1>
+
+const Welcome = (props) => {
+
+  return(
+    <Router>
+    <h1>WELCOME TO CLUELESS STYLING</h1>
+      <div>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route path="/login" component={LogIn}>
+            <LogIn submitHandler={props.login} />
+          </Route>
+          <Route path="/signup" component={SignUp}>
+            <SignUp submitHandler={props.signup} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default Welcome;  
