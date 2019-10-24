03df23f429400d2e24379ab3f658efe49cdab458
Belinda Lei <belinda.lei95@gmail.com>
2019-10-23 18:04:50 -0700

    form validations done

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,59 +1,83 @@
 import React from "react"
+import SimpleReactValidator from "simple-react-validator";
 
 class LogIn extends React.Component {
-  state = {
+  
+  constructor(props) {
+    super(props);
+    this.validator = new SimpleReactValidator();
+    this.state = {
       username: "",
       password: ""
+    };
   }
 
-  changeHandler = (e) => {
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+    if (this.validator.allValid()) {
+      this.props.submitHandler(this.state);
       this.setState({
-          [e.target.name]: e.target.value
-      })
-  }
-  submitHandler = (e) => {
-      e.preventDefault()
-      this.props.submitHandler(this.state)
-      this.setState({
-          name: "",
-          password: ""
-      })
-  }
+        name: "",
+        password: ""
+      });
+    } else {
+      this.validator.showMessages();
+      // rerender to show messages for the first time
+      // you can use the autoForceUpdate option to do this automatically`
+      this.forceUpdate();
+    }
+  };
 
   render() {
-      return (
-        <form className="ui small form" id="signup-login" onSubmit={this.submitHandler}>
-          <center>
-            <h1>Log In</h1>
-          </center>
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            placeholder="enter username"
-            onChange={this.changeHandler}
-          />
-          <br></br>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            placeholder="enter password"
-            onChange={this.changeHandler}
-          />
-          <br />
-          <br />
-          <center>
-            <input
-              className="ui inverted button"
-              type="submit"
-              value="submit"
-            />
-          </center>
-        </form>
-      );
+    return (
+      <form
+        className="ui small form"
+        id="signup-login"
+        onSubmit={this.submitHandler}
+      >
+        <center>
+          <h1>Log In</h1>
+        </center>
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          placeholder="enter username"
+          onChange={this.changeHandler}
+        />
+        {this.validator.message(
+          "username",
+          this.state.username,
+          "required|username"
+        )}
+        <br></br>
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          placeholder="enter password"
+          onChange={this.changeHandler}
+        />
+        {this.validator.message(
+          "password",
+          this.state.password,
+          "required|password"
+        )}
+        <br />
+        <br />
+        <center>
+          <input className="ui inverted button" type="submit" value="submit" />
+        </center>
+      </form>
+    );
   }
 }
 
